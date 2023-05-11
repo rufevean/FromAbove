d6c9225663ef31602deff695a49d6221350a6017
Leandro <leandroaraujo15998@gmail.com>
2023-05-11 15:25:25 -0300

    Menu initial styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,15 @@
 import { NavLink } from 'react-router-dom';
+import styles from './Header.module.css';
 
 export function Header() {
     return (
-        <div>
-            <img src="" alt="" />
+        <div className={styles.menuWrapper}>
+            <span>From Above</span>
             <nav>
                 <NavLink>Community</NavLink>
                 <NavLink>News</NavLink>
                 <NavLink>Register</NavLink>
-                <NavLink><button>login</button></NavLink>
+                <NavLink><button>Login</button></NavLink>
             </nav>
         </div>
     );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,10 @@
-import { Header } from "../../components/Header";
+import { Header } from "../../components/Header/Header";
+import '../../styles/global.css';
 
 export function Home() {
     return (
         <div>
             <Header />
-            teste
         </div>
     )
 }
